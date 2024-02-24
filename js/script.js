85cac55abd7830ad5cac3028d324e13972ba033a
Messeddy1 <mohamedesseddyqqy@gmail.com>
2024-02-24 01:46:20 +0100

    second

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let img = document.getElementById("img");
+let cont = document.querySelector(".container-fluid");
+function phons(phone) {
+  img.src = phone;
+}
+function colors(color) {
+  cont.style.background = color;
+}
